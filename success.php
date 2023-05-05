d84d8033f2cf57ab2a347c63688cfde13c5495cd
subrata-aurionpro <119386583+subrata-aurionpro@users.noreply.github.com>
2023-05-05 16:49:09 +0530

    Update success.php

--- success.php
+++ success.php
@@ -1,5 +1,24 @@
 <?php
-	echo 'Transaction Successful';
-?>
 
+	include('CryptAES.php');
+	$aes = new CryptAES();
+	$key = "8HF6K2eloDebD9ikXQmsig==";   // UAT
+
+	$secret=base64_decode($key);
+	$aes->set_key($secret);
+	// $aes->setKey($secret);
+	// $encData = $_REQUEST['encData'];
+	if ($_REQUEST['encData'])
+	{
+	// echo "<Br> Encrypted data = ".$_REQUEST['encData'];
+	// echo "<Br> Decrypted data = ". 
+		$encData = $aes->decrypt($_REQUEST['encData'], $key);
 	
+        echo json_encode(array(
+            'status' => true,
+            'encData' => $encData,
+        ));
+	} else {
+		die("Please try again...");
+	}
+ ?>
