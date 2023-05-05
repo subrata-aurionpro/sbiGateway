fc0f1fb64e652e50bdf00106e6b5d92e3f42d9ca
subrata-aurionpro <119386583+subrata-aurionpro@users.noreply.github.com>
2023-05-05 16:51:31 +0530

    Update failure.php

--- failure.php
+++ failure.php
@@ -1,3 +1,24 @@
 <?php
-    echo "Transaction failed.";
+    
+	include('CryptAES.php');
+    $aes = new CryptAES();
+    $key = "8HF6K2eloDebD9ikXQmsig==";   // UAT
+  
+   // $aes->set_key(base64_decode($key));
+   // $aes->require_pkcs5();
+    $secret=base64_decode($key);
+    $aes->set_key($secret);
+    if ($_REQUEST['encData'])
+    {
+        // echo "<Br> Encrypted data = ".$_REQUEST['encData'];
+        // echo "<Br> Decrypted data = ". 
+        $encData = $aes->decrypt($_REQUEST['encData'], $key);
+            
+        echo json_encode(array(
+            'status' => false,
+            'encData' => $encData,
+        ));
+    } else {
+        die("Please try again...");
+    }
 ?>
